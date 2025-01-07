bcf8b1766dbdf53b27417d64643312547864ef21
John T. Wodder II <git@varonathe.org>
2025-01-07 08:19:36 -0500

    Add testing against Python 3.13

--- src/mkissues/__init__.py
+++ src/mkissues/__init__.py
@@ -7,7 +7,7 @@ formatted with special headers.
 Visit <https://github.com/jwodder/mkissues> for more information.
 """
 
-__version__ = "0.1.0.dev"
+__version__ = "0.1.0.dev1"
 __author__ = "John T. Wodder II"
 __author_email__ = "mkissues@varonathe.org"
 __license__ = "MIT"
